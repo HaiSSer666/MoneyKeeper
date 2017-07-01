da69fed2e95e57e46a3d065861b1b6db350a5301
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-07-01 11:04:40 +0200

    add ID add usage of second Kost-constructor add deletion on ID add method public ArrayList<String> getListOfPieChartKosts() //test

--- src/main/java/com/aleksandrov/dao/KostDao.java
+++ src/main/java/com/aleksandrov/dao/KostDao.java
@@ -6,33 +6,35 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import com.aleksandrov.model.Kost;
 import com.aleksandrov.model.SpendType;
-import com.aleksandrov.utils.connection;  
+import com.aleksandrov.utils.connection;
 
 public class KostDao {  
 	private static Statement statement = connection.createStatement();
 
 	public void insertKost(Kost kost) throws SQLException{  
-		String query="INSERT INTO 'Kost' ('amount', 'category', 'spendType', 'dateOfPurchaseOrIncome', 'creationDate', 'comment') "
-				+ "VALUES('"+kost.getAmount()+"','"+kost.getCategory()+"','"+kost.getSpendType()+"','"+kost.getDateOfPurchaseOrIncome()+"','"+kost.getCreationDate()+"','"+kost.getComment()+"')"; 
+		String query="INSERT INTO 'Kost' ('ID', 'amount', 'category', 'spendType', 'dateOfPurchaseOrIncome', 'creationDate', 'comment') "
+				+ "VALUES('"+kost.getId()+"','"+kost.getAmount()+"','"+kost.getCategory()+"','"+kost.getSpendType()+"','"+kost.getDateOfPurchaseOrIncome()+"','"+kost.getCreationDate()+"','"+kost.getComment()+"')"; 
 		statement.execute(query);
 	}  
 
-	
-	
 	public List<Kost> getAllKosts(){
 		List<Kost> kosts = new ArrayList<>();
 		try {
 			ResultSet resSet = statement.executeQuery("SELECT * FROM Kost");
 			while(resSet.next())
 			{
+				String id = resSet.getString("ID");
+				UUID uid = UUID.fromString(id);
 				float amount = resSet.getFloat("amount");
 				String  category = resSet.getString("category");
 				String  dbSppendType = resSet.getString("spendType");
-				String  comment = resSet.getString("comment");
 				String  dateStamp = resSet.getString("dateOfPurchaseOrIncome");//костыль?
 			    LocalDate dateOfPurchaseOrIncome = LocalDate.parse(dateStamp);
+			    String  creationDate = resSet.getString("creationDate");
+			    String  comment = resSet.getString("comment");
 				SpendType spendType;
 				if(dbSppendType.equals("KOST")){
 					spendType=SpendType.KOST;
@@ -40,7 +42,7 @@ public class KostDao {
 				else{
 					spendType=SpendType.GAIN;
 				}
-				Kost kost = new Kost(amount, category, spendType, dateOfPurchaseOrIncome, comment);
+				Kost kost = new Kost(uid, amount, category, spendType, dateOfPurchaseOrIncome, creationDate, comment);
 				kosts.add(kost); 
 			}
 		} catch (SQLException e) {
@@ -49,6 +51,32 @@ public class KostDao {
 		return kosts;
 	}
 
+	public double getTotalAmount(SpendType spendType) {
+		double totalAmount = 0;
+		for(Kost kost : getAllKosts()){
+			if(kost.getSpendType()==spendType){
+				totalAmount+=kost.getAmount();
+			}
+		}
+		return totalAmount;
+	}
+	
+	public ArrayList<String> getListOfPieChartKosts() {
+		ArrayList<String> listOfKosts = new ArrayList<String>();
+		for(Kost kost : getAllKosts()){
+			if(kost.getSpendType()==SpendType.KOST){
+				String category = kost.getCategory();
+				listOfKosts.add(category);
+			}
+		}
+		return listOfKosts;
+	}
+	
+	public void deleteKost(UUID id) throws SQLException{  
+		String query="DELETE FROM 'Kost' WHERE ID='"+id+"'"; 
+		statement.execute(query);
+	}
+	
 	public void deleteDbKost() throws SQLException{  
 		String query="DELETE FROM 'Kost'";  
 		statement.execute(query);
