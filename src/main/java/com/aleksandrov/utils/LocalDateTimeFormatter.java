a9ebfa3fd9fed9b85b805c95268365ea1c6aea8b
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-07-01 10:57:13 +0200

    set buy/spend date with seconds

--- src/main/java/com/aleksandrov/utils/LocalDateTimeFormatter.java
+++ src/main/java/com/aleksandrov/utils/LocalDateTimeFormatter.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeFormatter {
 
 	public static String formateDate (LocalDateTime unformatedDate){
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formatDateTime = unformatedDate.format(formatter);
         return formatDateTime;
 	}
