c40f05873c2ed00815b701d2ec4b4368cc6a1de0
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-07-01 10:57:51 +0200

    add ID add second constructor for reading elements from DB

--- src/main/java/com/aleksandrov/model/Kost.java
+++ src/main/java/com/aleksandrov/model/Kost.java
@@ -2,17 +2,21 @@ package com.aleksandrov.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
+
 import com.aleksandrov.utils.LocalDateTimeFormatter;
 
 public class Kost {
+	private UUID id;
 	public double amount;
 	public String category;
 	SpendType spendType;
 	public String creationDate;
 	public LocalDate dateOfPurchaseOrIncome;
 	public String comment; 
-
+	
 	public Kost(double amount, String category, SpendType spendType, LocalDate dateOfPurchaseOrIncome, String comment){
+		this.id=UUID.randomUUID();
 		this.amount=amount;
 		this.category=category;
 		this.spendType=spendType;
@@ -20,7 +24,21 @@ public class Kost {
 		this.creationDate=LocalDateTimeFormatter.formateDate(LocalDateTime.now());
 		this.comment=comment;
 	}
+	
+	public Kost(UUID id, double amount, String category, SpendType spendType, LocalDate dateOfPurchaseOrIncome, String creationDate, String comment){
+		this.id=id;
+		this.amount=amount;
+		this.category=category;
+		this.spendType=spendType;
+		this.dateOfPurchaseOrIncome=dateOfPurchaseOrIncome;
+		this.creationDate=creationDate;
+		this.comment=comment;
+	}
 
+	public UUID getId() {
+		return id;
+	}
+	
 	public String getCategory() {
 		return category;
 	}
