202f0464cecff5d300af986bedc67b01b9f12142
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-07-04 19:22:58 +0200

    add search menu controls add handleSearchButton()

--- src/main/java/com/aleksandrov/controller/MainController.java
+++ src/main/java/com/aleksandrov/controller/MainController.java
@@ -8,23 +8,49 @@ package com.aleksandrov.controller;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Month;
-
+import java.util.HashSet;
 import com.aleksandrov.Main;
 import com.aleksandrov.dao.KostDao;
 import com.aleksandrov.model.Kost;
 import com.aleksandrov.model.SpendType;
 import javafx.beans.binding.BooleanBinding;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class MainController {	
+	@FXML ComboBox<String> comboBoxCategory;
+	@FXML ComboBox<String> comboBoxMonths;
+	@FXML ComboBox<Integer> comboBoxYears;
+	HashSet<String> listOfKosts = new HashSet<String>();
+	ObservableList<Integer> listOfYears = FXCollections.observableArrayList(
+		        2017,
+		        2018,
+		        2019
+		    );
+	@FXML TextField textFieldResult;
+	@FXML Button searchButton;
+	
+	@FXML
+	public void handleSearchButton() {
+		try {
+			double monthlyTotalCategoryAmount = kostDao.getTotalCategoryAmount(comboBoxCategory.getValue());
+			System.out.println(monthlyTotalCategoryAmount);
+			textFieldResult.setText(String.valueOf(monthlyTotalCategoryAmount)+" Euro");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	//link to dao
 	KostDao kostDao = new KostDao();
 
@@ -64,6 +90,11 @@ public class MainController {
 
 	@FXML
 	private void initialize() {	
+		listOfKosts = kostDao.getListOfPieChartKosts();
+		comboBoxCategory.getItems().addAll(listOfKosts);
+		comboBoxMonths.getItems().addAll(kostsBarChartController.getMonthsOfYear());
+		comboBoxYears.getItems().addAll(listOfYears);
+		
 		//restoting data with dao
 		totalAmountKost = kostDao.getTotalAmount(SpendType.KOST);
 		totalAmountGain = kostDao.getTotalAmount(SpendType.GAIN);
